c4d2930d098c05c4891b4af76715ba88eb42da09
Your Name <you@example.com>
2024-05-17 20:11:47 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -60,14 +60,6 @@ int is_valid(Node* n){
 
          //Verificacion submatriz
 
-         int x = i/3;
-         int y = j/3;
-         
-         int k=4,p; 
-         for(p=0;p<9;p++){
-             int i=3*(k/3) + (p/3) ;
-             int j=3*(k%3) + (p%3) ;
-             if(p%3 == 2) printf("\n");
          }
       }
    }
