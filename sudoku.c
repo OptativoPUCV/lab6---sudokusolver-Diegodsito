a964652fbf90858883252c7e75c6753489c4400b
Your Name <you@example.com>
2024-05-18 23:42:21 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -50,9 +50,11 @@ int is_valid(Node* n){
          
          if(n->sudo[i][j] == 0) continue;
          for(int k = 0 ; k < 9 ; k++){
+            if(k == j) continue;
             if(n->sudo[i][k] == n->sudo[i][j]) return 0;
          }
          for(int k = 0 ; k < 9 ; k++){
+            if(i == k) continue;
             if(n->sudo[k][j] == n->sudo[i][j]) return 0;
          }
       }
